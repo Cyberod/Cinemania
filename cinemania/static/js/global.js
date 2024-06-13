3bc3321b5a794e981ef624976315716580f83f84
Cyberod <jontkeez@gmail.com>
2024-06-13 08:26:55 +0100

    still fixing dropdown issues

--- cinemania/static/js/global.js
+++ cinemania/static/js/global.js
@@ -15,25 +15,12 @@ const addEventOnElements = function (elements, eventType, callback) {
 
 const searchBox = document.querySelector("[search-box]");
 const searchTogglers = document.querySelectorAll("[search-toggler]");
-const subMenu = document.querySelector
-const subBtn = document.querySelectorAll("[sub-btn]");
-const dropdown = document.querySelector("[dropdown]");
 
 addEventOnElements(searchTogglers, "click", function() { 
     searchBox.classList.toggle("active");
     subBtn.classList.toggle("active");
     dropdown.classList.toggle("active");
  });
-
-
-
-
-/*const exploreBtn = document.querySelector('.explore-btn');
-const exploreMenu = document.querySelector('.explore-menu');
-
-exploreBtn.addEventListener('click', () => {
-  exploreMenu.classList.toggle('show'); 
-})*/
 
 
 
